f6226e24434d4dc8605d87ed3b4aa371f55abe5d
alita <alitakendrick@Alitas-MacBook-Pro.local>
2017-06-13 20:40:43 -0500

    starting server file

--- server.js
+++ server.js
@@ -20,3 +20,29 @@ app.use(bodyParser.json({type: "application/vnd.api+json"}));
 
 app.use(express.static("./public"));
 
+
+//mongodb config
+mongoose.connect("mongodb://localhost/homework19");
+var db = mongoose.connection;
+
+db.on("error", function(err) {
+	console.log("Mongoose Error: ", err);
+});
+
+db.once("open", function() {
+	console.log("Mongoose connection successful");
+});
+
+
+//main route to redirect to rendered react app
+app.get("/", function(req, res) {
+	res.sendFile(__dirname + "/public/index.html");
+});
+
+
+
+
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
+})
+
