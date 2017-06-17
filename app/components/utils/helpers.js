861576333f7f1f7dda89ea3bf6f2f4593c1ef9b1
alita <alitakendrick@Alitas-MacBook-Pro.local>
2017-06-17 09:25:46 -0500

    ak

--- app/components/utils/helpers.js
+++ app/components/utils/helpers.js
@@ -1,15 +1,9 @@
-// Include the axios package for performing HTTP requests (promise based alternative to request)
 var axios = require("axios");
-
-// Helper functions for making API Calls
 var helper = {
-
-  // This function serves our purpose of running ajax query to get nyt articles.
-  runQuery: function(topic, yearStart, yearEnd) {
-
-    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+ runQuery: function(topic, yearStart, yearEnd) {
+   var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
     url += '?' + $.param({
-        'api-key': "b9f91d369ff59547cd47b931d8cbc56b:0:74623931",
+        'api-key': "97465cece832491fa3bc26a21108435b",
         'q': topic,
         'begin_date': yearStart + "0101",
         'end_date': yearEnd + "0101",
@@ -31,16 +25,19 @@ var helper = {
         }    return articleArray;
       }    return "";
     });
-  },  getSavedArticles: function() {
+  },  
+
+  getSavedArticles: function() {
     return axios.get("/api/saved");
   },
+  
   postSavedArticles: function(article) {
     return axios.post("/api/saved", { article: article });
   },
+
   deleteSavedArticle: function(article) {
     return axios.delete("/api/saved", { data: {article: article }});
   }
 };
 
-// We export the API helper
 module.exports = helper;
